ad290087e5b1e61b5cbd6550d8ff9115c43f3aa9
Oscar Le Dauphin <oscar.le-dauphin@epita.fr>
2025-03-27 22:20:03 +0100

    feat(tests): property based testing using proptest

--- tests/mocked_client_proptest.rs
+++ tests/mocked_client_proptest.rs
@@ -8,20 +8,47 @@ use async_lsp::lsp_types::{
     TextDocumentContentChangeEvent, TextDocumentItem, Url,
 };
 use common::lsp_client;
-use proptest::{prelude::Arbitrary, proptest};
+use proptest::{prelude::Arbitrary, prop_compose, proptest, test_runner::TestRunner};
 use proptest_derive::Arbitrary;
 use std::{env::temp_dir, process::Command, time::Duration};
 use tracing::Level;
 
-#[derive(Debug)]
-struct TestTextDocumentContentChangeEvent(TextDocumentContentChangeEvent);
+const MAX_POSITION_SIZE: u32 = 10;
+const INPUT_REGEX: &str = "a{1,256}";
 
-impl Arbitrary for TestTextDocumentContentChangeEvent {
-    type Parameters = (String);
-    type Strategy = ();
+prop_compose! {
+    fn arb_text_document_change()(
+        input      in INPUT_REGEX,
+        start_line in 0..MAX_POSITION_SIZE,
+        start_char in 0..MAX_POSITION_SIZE,
+        end_line   in 0..MAX_POSITION_SIZE,
+        end_char   in 0..MAX_POSITION_SIZE
+    ) -> TextDocumentContentChangeEvent {
+        TextDocumentContentChangeEvent {
+            range: Some(Range {
+                start: Position {
+                    line: start_line,
+                    character:start_char
+                },
+                end: Position { line:
+                    end_line, character:
+                    end_char
+                }
+            }),
+            range_length: None,
+            text: input,
+        }
+    }
+}
 
-    fn arbitrary_with(args: Self::Parameters) -> Self::Strategy {
-        todo!()
+#[derive(Arbitrary, Debug)]
+struct TextDocumentContentChangeEventWrap(
+    #[proptest(strategy = "arb_text_document_change()")] TextDocumentContentChangeEvent,
+);
+
+impl From<TextDocumentContentChangeEventWrap> for TextDocumentContentChangeEvent {
+    fn from(val: TextDocumentContentChangeEventWrap) -> Self {
+        val.0
     }
 }
 
@@ -30,11 +57,12 @@ enum Client {
     Client1,
     Client2,
 }
+
 // single document change from one of the clients
 #[derive(Debug, Arbitrary)]
 struct ClientChange {
     from: Client,
-    change: TestTextDocumentContentChangeEvent,
+    changes: Vec<TextDocumentContentChangeEventWrap>,
 }
 
 #[derive(Debug, Arbitrary)]
@@ -43,25 +71,12 @@ struct TestCase {
 }
 
 async fn test_mocked_clients_quickcheck(params: TestCase) -> anyhow::Result<()> {
-    tracing_subscriber::fmt()
-        .with_max_level(Level::INFO)
-        .pretty()
-        .with_writer(std::io::stderr)
-        .init();
-
-    let mut _server_child =
-        async_process::Command::from(Command::cargo_bin("server").expect("server binary to exist"))
-            .kill_on_drop(true)
-            .spawn()
-            .expect("could not spawn server");
-
     let work_dir = temp_dir();
     let mut client1 = lsp_client::MockClient::new().await;
-    let client2 = lsp_client::MockClient::new().await;
+    let mut client2 = lsp_client::MockClient::new().await;
 
     let file_uri = Url::from_file_path(work_dir.join("src/lib.rs")).unwrap();
     let text = ""; // TODO: send open document to others for initial content
-    let added = "test";
     client1.did_open(DidOpenTextDocumentParams {
         text_document: TextDocumentItem {
             uri: file_uri.clone(),
@@ -72,29 +87,23 @@ async fn test_mocked_clients_quickcheck(params: TestCase) -> anyhow::Result<()> 
     })?;
 
     for change in params.changes {
-        // client1
-        //     .did_change(DidChangeTextDocumentParams {
-        //         text_document: async_lsp::lsp_types::VersionedTextDocumentIdentifier {
-        //             uri: file_uri.clone(),
-        //             version: 0,
-        //         },
-        //         content_changes: vec![TextDocumentContentChangeEvent {
-        //             range: Some(Range::new(Position::new(0, 0), Position::new(0, 0))),
-        //             text: added.to_owned(),
-        //             range_length: None,
-        //         }],
-        //     })
-        //     .await?;
-        todo!()
+        match change.from {
+            Client::Client1 => &mut client1,
+            Client::Client2 => &mut client2,
+        }
+        .did_change(DidChangeTextDocumentParams {
+            text_document: async_lsp::lsp_types::VersionedTextDocumentIdentifier {
+                uri: file_uri.clone(),
+                version: 0,
+            },
+            content_changes: change.changes.into_iter().map(|c| c.into()).collect(),
+        })
+        .await?;
     }
 
     // this is not great
-    tokio::time::sleep(Duration::from_millis(10)).await;
+    tokio::time::sleep(Duration::from_millis(50)).await;
 
-    // let expected = format!("{}{}", added, text);
-    // assert_eq!(client1.document(), expected);
-    // assert_eq!(client2.document(), expected);
-    assert!(!client1.document().is_empty());
     assert_eq!(client1.document(), client2.document());
 
     client1.drop().await;
@@ -102,8 +111,29 @@ async fn test_mocked_clients_quickcheck(params: TestCase) -> anyhow::Result<()> 
     Ok(())
 }
 
-proptest! {
-    #[test]
-    fn test_mocked_clients_quickcheck_sync(params: TestCase) {dbg!(client1_changes);
-    }
+#[test]
+fn test_mocked_clients_quickcheck_sync() -> proptest::test_runner::TestCaseResult {
+    let mut _server_child =
+        async_process::Command::from(Command::cargo_bin("server").expect("server binary to exist"))
+            .kill_on_drop(true)
+            .spawn()
+            .expect("could not spawn server");
+    let mut runner = TestRunner::default();
+    tracing_subscriber::fmt()
+        .with_max_level(Level::INFO)
+        .pretty()
+        .with_writer(std::io::stderr)
+        .init();
+    let tokio_runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_time()
+        .build()
+        .unwrap();
+
+    runner.run(&TestCase::arbitrary(), |test_params| {
+        tokio_runtime
+            .block_on(async move { test_mocked_clients_quickcheck(test_params).await })
+            .expect("test to pass");
+        Ok(())
+    })?;
+    Ok(())
 }
